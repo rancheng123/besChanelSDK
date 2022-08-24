7d2e832d85c909f4f814e15d7ec37da51afc96d2
rancheng <rancheng@scrmtech.com>
2022-08-24 19:15:54 +0800

    24

--- publish.dev.sh
+++ publish.dev.sh
@@ -1,3 +1,16 @@
-docker build -t sdk-stg:0.0.1 -f ./Dockerfile . --build-arg env=dev
 
-docker run -detach --publish 80:80 --name sdk-stg-container sdk-stg:0.0.1
+docker build --build-arg env=dev -t sdk-dev:0.0.1         -f ./Dockerfile .
+docker tag sdk-dev:0.0.1 zqbc-scrm-new-docker.pkg.coding.net/renew/test/sdk-dev:0.0.1
+docker push zqbc-scrm-new-docker.pkg.coding.net/renew/test/sdk-dev:0.0.1
+
+
+
+ssh root@10.0.0.49 -p 22
+
+
+
+docker pull zqbc-scrm-new-docker.pkg.coding.net/renew/test/sdk-dev:0.0.1
+docker run -d -p 9501:80 zqbc-scrm-new-docker.pkg.coding.net/renew/test/sdk-dev:0.0.1
+
+
+docker run -detach --publish 80:80 --name sdk-dev-container sdk-dev:0.0.1
